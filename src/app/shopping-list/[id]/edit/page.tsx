ab2ec73a5a73bd4e2643835abdc307701271e7c9
mukolwesofts <mukolwesofts@gmail.com>
2025-05-10 13:23:42 +0300

    remove unsused files

--- src/app/shopping-list/[id]/edit/page.tsx
+++ src/app/shopping-list/[id]/edit/page.tsx
@@ -1,8 +1,7 @@
-export default function ShoppingListEditPage() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold text-teal-700 mb-4">Edit Shopping Item</h1>
-            <p>This is the shopping list edit form page.</p>
-        </div>
-    );
-}
+export default function EditShoppingListPage() {
+  return (
+    <div>
+      <h1>Edit Shopping List</h1>
+    </div>
+  );
+} 
--- src/app/shopping-list/create/page.tsx
+++ src/app/shopping-list/create/page.tsx
@@ -1,8 +1,7 @@
-export default function ShoppingListCreatePage() {
-    return (
-        <div>
-            <h1 className="text-2xl font-bold text-teal-700 mb-4">Add New Shopping Item</h1>
-            <p>This is the shopping list create form page.</p>
-        </div>
-    );
-}
+export default function CreateShoppingListPage() {
+  return (
+    <div>
+      <h1>Create Shopping List</h1>
+    </div>
+  );
+} 
